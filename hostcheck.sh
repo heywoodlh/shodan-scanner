a7522be0888b2dcb8ac75686fdefd3cc0764f6b5
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-07-12 22:26:22 -0600

    updated scripts

--- hostcheck.sh
+++ hostcheck.sh
@@ -1,27 +1,69 @@
 #!/usr/bin/env bash
 #Bash script for regularly checking on specific hosts to see if they have been scanned and posted on Shodan
 
+#If you would like to enable slack notification please configure the slack variables in auth.py
+
+AUTH_STATE="$(cat auth.py | grep "api_key")"
+if [[ "$AUTH_STATE" == 'api_key = ""' ]]
+then
+	echo "please configure an api key in auth.py"
+	exit 1
+fi
+
 GROUP="$(cat hosts.txt)"
 
 shodan_scan() {
 	DATE="$(date +%D\ %H:%M:%S)"
 	echo "$DATE"
 	echo "$HOSTNAME"
-	./shodan host "$HOSTS"
+	./shodan host "$IND_HOSTS"
 	printf "\n"
 	printf "\n"
 }
 
 HOSTNAME="$HOSTS"
 
-for HOSTS in "$GROUP"
+if [[ -f shodanhostscan.log ]]
+then
+	OLD_LOGS="$(cat shodanhostscan.log)"
+	rm shodanhostscan.log
+fi
+
+for IND_HOSTS in "$GROUP"
 do
-	if [[ "$HOSTS" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] 
+	if [[ "$IND_HOSTS" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] 
 	then
-	       shodan_scan >> shodanhostscan.log
+		NEW_LOGS="$(shodan_scan)"
+		if [[ "$NEW_LOGS" != "$OLD_LOGS" ]]
+		then 
+			NOTIFY="true"
+		else
+			NOTIFY="false" 
+		fi
+
+		echo "$NEW_LOGS" >> shodanhostscan.log
 	else
-	       HOSTNAME="$HOSTS"
-	       HOSTS=$(dig +short $HOSTS)
-	       shodan_scan >> shodanhostscan.log
-        fi	       
+	        HOSTNAME="$IND_HOSTS"
+	        IND_HOSTS="$(dig +short $IND_HOSTS)"
+		NEW_LOGS="$(shodan_scan)"
+
+	        if [[ "$NEW_LOGS" != "$OLD_LOGS" ]]
+	        then 
+	       		NOTIFY="true"
+	        else
+			NOTIFY="false" 
+	        fi
+
+		echo "$NEW_LOGS" >> shodanhostscan.log
+        fi	  	
 done
+
+slack_notifications() {
+	./slack-notify.py
+}
+
+if [[ "$NOTIFY" == "true" ]] && [[ "$SLACK_API" != "" ]] && [[ "$CHANNEL" != "" ]] 
+then
+	slack_notifications
+fi
+
